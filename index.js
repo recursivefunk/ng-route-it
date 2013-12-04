e887bb5748a55484ba81ef1eb4332a076794127f
johnny-ray <johnny@johnnyray.me>
2013-12-04 16:23:28 -0500

    bug fixes

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
 
+
 module.exports = function(req) {
+  var obj = req.path.split('/');
+
   var str = '/#';
-  for(var i in req.params) {
-    str += '/' + req.params[ i ];
+
+  for(var i in obj) {
+    var tmp = obj[ i ];
+    if (tmp) {
+      if ( tmp[ 0 ].indexOf(':') === 0 ) {
+        var length = tmp.length;
+        tmp = tmp.substring( 1, length - 1 );
+      }
+      str += '/' + tmp;
+    }
   }
   return str;
 }
